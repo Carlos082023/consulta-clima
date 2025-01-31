a2c294c9b48dcaca5ffa013b7711660b788a438f
Carlos Vladislavic <Carlos082023@users.noreply.github.com>
2025-01-31 13:12:11 -0300

    Terminacion de la Aplicacion, Mejora del CSS y el Readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# consulta-clima
+# 🌤️ Aplicación de Clima
+
+Esta es una aplicación web que permite consultar el clima de cualquier ciudad en tiempo real utilizando la API de OpenWeatherMap. 🏙️☀️🌧️
+
+## 🚀 Características
+
+✅ Búsqueda de clima por nombre de ciudad  
+✅ Muestra temperatura, sensación térmica, humedad y descripción del clima  
+✅ Iconos representativos del clima  
+✅ Diseño moderno con colores dinámicos  
+
+## 🖥️ Captura de Pantalla
+![Vista previa de la aplicación](clima-aplicacion.png)
+
+
+
+## 🔧 Tecnologías Utilizadas
+
+- **HTML5**  
+- **CSS3**  
+- **JavaScript (ES6+)**  
+- **API OpenWeatherMap**  
+
+## 📌 Instalación y Uso
+
+1. **Clonar el repositorio**
+   ```sh
+   git clone https://github.com/Carlos082023/consulta-clima.git
--- script.js
+++ script.js
@@ -16,5 +16,40 @@ document.getElementById('searchButton').addEventListener('click', ()=>{
 function fetchClima(city){
     fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
     .then(data => data.json())
-    .then(data => console.log(data))
+    .then(data => showWeatherData(data))
+}
+
+function showWeatherData(data){
+    const divResponse = document.getElementById('responseData')
+    divResponse.innerHTML = ''
+
+    const cityName = data.name
+    const  countryName = data.sys.country
+    const temperatura = data.main.temp
+    const SensacionTermica = data.main.feels_like
+    const humidity = data.main.humidity
+    const description = data.weather[0].description
+    const icon = data.weather[0].icon
+
+    const cityInfo = document.createElement('h2')
+    cityInfo.textContent = `${cityName}, ${countryName}`
+
+    const tempInfo = document.createElement('p')
+    tempInfo.textContent = `Temperatura: ${Math.floor(temperatura-diffKelvin)}°C ST: ${Math.floor(SensacionTermica-diffKelvin)}°C`
+
+    const humidityInfo = document.createElement('p')
+    humidityInfo.textContent = `humedad del ${humidity}%`
+
+    const iconInfo = document.createElement('img')
+    iconInfo.src = ` https://openweathermap.org/img/wn/${icon}@2x.png`
+
+    const descriptionInfo = document.createElement('p')
+    descriptionInfo.textContent = `Descripcion es: ${description}`
+
+    divResponse.appendChild(cityInfo)
+    divResponse.appendChild(tempInfo)
+    divResponse.appendChild(humidityInfo)
+    divResponse.appendChild(iconInfo)
+    divResponse.appendChild(descriptionInfo)
+
 }
